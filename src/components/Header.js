1cf84692632377421c1b72b3d2851da5a06f0400
Eric Yager <eyager@amazon.com>
2021-05-09 19:50:24 -0500

    updated base page information and title, added header link to github repo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import {
     MDBNavbar,
-    MDBNavbarNav
+    MDBNavbarNav,
+    MDBIcon,
+    MDBBtn
   } from 'mdbreact';
   import React, { useState, useEffect } from "react";
   import '../static/css/Header.css';
@@ -19,10 +21,14 @@ import {
         <MDBNavbar color='elegant-color' className='border-primary border-bottom border-top-0 border-left-0 border-right-0' dark expand='md' fixed='top' scrolling>
             <MDBNavbarNav left>
                 <nav class="nav nav-pills flex-column flex-sm-row">
-                    <a class={currentWindow=="" ? activeClass : inactiveClass} href="/">Home</a>
-                    <a class={currentWindow=="Resume" ? activeClass : inactiveClass} href="/Resume">Resume</a>
-                    <a class={currentWindow=="Statistics" ? activeClass : inactiveClass} href="#">Statistics</a>
+                    <a className={currentWindow=="" ? activeClass : inactiveClass} href="/">Home</a>
+                    <a className={currentWindow=="Resume" ? activeClass : inactiveClass} href="/Resume">Resume</a>
+                    <a className={currentWindow=="Statistics" ? activeClass : inactiveClass} href="#">Statistics</a>
                 </nav>
+            </MDBNavbarNav>
+            <MDBNavbarNav right>
+                <a href="https://github.com/ericYagerPractice/resume" target="_blank" className="btn btn-sm btn-dark btn-rounded">Check out repo &nbsp;<MDBIcon size="1x" className="text-light" icon="code-branch" /></a>
+                
             </MDBNavbarNav>
         </MDBNavbar>
         
